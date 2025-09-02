9e48f12942a5c09a555880eccd035558ae405fe0
dev <dev1672004@gmail.com>
2025-09-02 17:31:51 +0530

    active model

---
+++ app/models/applicant.rb
@@ -0,0 +1,8 @@
+class Applicant
+  include ActiveModel::SecurePassword
+
+  has_secure_password
+  has_secure_password :recovery_password, validations: false
+
+  attr_accessor :password_digest, :recovery_password_digest
+end
--- db/migrate/20250828044413_create_books.rb
+++
@@ -1,7 +0,0 @@
-class CreateBooks < ActiveRecord::Migration[8.0]
-  def change
-    create_table :books do |t|
-      t.timestamps
-    end
-  end
-end
